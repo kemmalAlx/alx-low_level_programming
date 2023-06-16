24aa1ba4b10063cd48ebea7d2f5ee767be9fbd77
kemmalAlx <kemmalalx@gmail.com>
2023-06-15 17:42:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+
 int ft_strlen(char *str)
 {
 	int i = -1;
