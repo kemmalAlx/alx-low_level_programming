aeeeee7c6e3ca2ac9f25cb4e9ce53187724b23f1
kemmalAlx <kemmalalx@gmail.com>
2023-06-16 07:35:38 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alpabect in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
