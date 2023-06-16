06f6103701b70f3ecc014438b21c24e32087e787
kemmalAlx <kemmalalx@gmail.com>
2023-06-15 17:43:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,7 @@
 int ft_strlen(char *str)
 {
 	int i = -1;
+
 	while (str[++i]);
 	return (i);
 }
@@ -19,6 +20,7 @@ int ft_strlen(char *str)
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, &str, ft_strlen(str));
 	return (1);
 }
