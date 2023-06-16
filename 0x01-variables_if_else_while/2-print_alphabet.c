391d3d021b98c68bbd323165af4e5c561b1c7f45
kemmalAlx <kemmalalx@gmail.com>
2023-06-16 07:24:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alpabect in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    int c = 'a';
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+    return (0);
+}
+
