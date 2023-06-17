c704971d2b2dc3303a21b0f3b520c9af59ab4b65
abdel-ke <kemmal1337@gmail.com>
2023-06-17 02:39:26 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,28 +10,28 @@
 
 int main(void)
 {
-    int i = 0;
-    int j;
+	int i = 0;
+	int j;
 
-    while (i <= 98)
-    {
-        j = i + 1;
-        while (j <= 99)
-        {
-            putchar((i / 10) + 48);
-            putchar((i % 10) + 48);
-            putchar(' ');
-            putchar((j / 10) + 48);
-            putchar((j % 10) + 48);
-            if (i != 98 || j != 99)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            j++;
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
