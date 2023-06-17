e17cdb5501f14ddab82be53da194214a018329bb
abdel-ke <kemmal1337@gmail.com>
2023-06-17 02:37:25 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i = 0;
+    int j;
+
+    while (i <= 98)
+    {
+        j = i + 1;
+        while (j <= 99)
+        {
+            putchar((i / 10) + 48);
+            putchar((i % 10) + 48);
+            putchar(' ');
+            putchar((j / 10) + 48);
+            putchar((j % 10) + 48);
+            if (i != 98 || j != 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+            j++;
+        }
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
