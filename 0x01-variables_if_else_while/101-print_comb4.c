509c13469cb3b5fe6c2ec364aae3d4dacff2f80c
abdel-ke <kemmal1337@gmail.com>
2023-06-17 02:18:00 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,32 +10,32 @@
 
 int main()
 {
-    int i = 0;
-    int j;
-    int k;
+	int i = 0;
+	int j;
+	int k;
 
-    while (i <= 7)
-    {
-        j = i + 1;
-        while (j <= 8)
-        {
-            k = j + 1;
-            while (k <= 9)
-            {
-                putchar(i + 48);
-                putchar(j + 48);
-                putchar(k + 48);
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
