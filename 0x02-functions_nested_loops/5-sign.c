4c8474e5e2f2b46a14200cda25821f695b7795dc
abdel-ke <kemmal1337@gmail.com>
2023-06-21 20:30:48 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * @c: checks input of function
  *
  * Return: 1 and prints + if n is greater than zero
- * 		 0 and prints 0 if n is zero
+ * 		0 and prints 0 if n is zero
  * 		-1 and prints - if n is less than zero
  */
 
