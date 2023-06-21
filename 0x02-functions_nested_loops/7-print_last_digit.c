6d9aca6f80bd2875f281b01e2d002411a2f8e9a8
abdel-ke <kemmal1337@gmail.com>
2023-06-21 20:39:31 +0100

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that computes the last
+ *       digit
+ *
+ * @nbr: takes in integer type input for function
+ *
+ * Return: last digit of nbr
+*/
+
+int print_last_digit(int nbr)
+{
+	return (nbr / 10);
+}
