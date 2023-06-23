16315f24f86786313ef213c1534a765d403c975c
abdel-ke <kemmal1337@gmail.com>
2023-06-24 00:33:46 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,11 +9,12 @@
 void print_number(int n)
 {
 	unsigned int nbr = n;
+
 	if (n < 0) {
 		_putchar('-');
 		nbr = -nbr;
 	}
-	if ((nbr / 10) > 1)
+	if ((nbr) >= 10)
 		print_number(nbr / 10);
 	_putchar((nbr % 10) + '0');
 }
