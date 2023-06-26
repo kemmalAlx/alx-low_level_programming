c755a975b2fe1a9748ef289c325857cf4fc59c78
abdel-ke <kemmal1337@gmail.com>
2023-06-26 18:29:52 +0100

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @s: the input
+*/
+
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+		i++;
+	}
+}
