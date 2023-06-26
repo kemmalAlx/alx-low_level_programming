be1b92c7364aa3343e859ca1a4e51a5985918038
abdel-ke <kemmal1337@gmail.com>
2023-06-26 17:13:30 +0100

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two numbers
+ *
+ * @s: first input
+ * Return: length of s
+*/
+
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return i;
+}
