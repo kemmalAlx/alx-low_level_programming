4979766871047d5acd1fb5e26efeb51ac989454a
abdel-ke <kemmal1337@gmail.com>
2023-07-07 22:08:16 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- a program that prints its name
+ *
+ * @ac: holds the number of arguments passed
+ * @av: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int ac, char **av)
+{
+	printf("%s\n", av[0]);
+	return (0);
+}
