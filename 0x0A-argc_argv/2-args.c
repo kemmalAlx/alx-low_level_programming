813463057ce164b8d819abfdf9e1d8e260d85001
abdel-ke <kemmal1337@gmail.com>
2023-07-07 22:40:39 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a program that prints all arguments it receives
+ *
+ * @ac: holds the number of arguments passed
+ * @av: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int ac, char **av)
+{
+	int i = 0;
+
+	while(i < ac)
+		printf("%s\n", av[i++]);
+	return (0);
+}
