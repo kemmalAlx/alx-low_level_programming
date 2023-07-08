b31a569b5c7d984a621394d6a6b0f0c5dc3275fb
abdel-ke <kemmal1337@gmail.com>
2023-07-08 02:58:31 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - a program that prints the minimum number of coins 
+ *
+ * @ac: holds the number of arguments passed
+ * @av: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int ac, char **av)
+{
+	int num = atoi(av[1]), j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (ac != 2)
+		return (printf("Error\n"), 1);
+	if (num < 0)
+		printf("0\n");
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
