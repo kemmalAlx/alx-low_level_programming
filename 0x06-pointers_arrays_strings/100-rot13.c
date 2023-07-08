99a1a8e95f22737b32fd9a3285655d8331cae97b
abdel-ke <kemmal1337@gmail.com>
2023-07-09 00:50:41 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,7 +3,7 @@
 /**
  * rot13 - Write a function that encodes a string using rot13
  *
- * @hi: This is my input string
+ * @str: This is my input string
  *
  * Return: String converted to rot13
  *
@@ -12,18 +12,19 @@
 char *rot13(char *str)
 {
 	int i = 0;
-	// int upper = 
-	// char lower[] = 
+
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'm' || str[i] >= 'A' && str[i] <= 'M')
+		if (str[i] >= 'a' && str[i] <= 'm'
+		|| str[i] >= 'A' && str[i] <= 'M')
 			str[i] += 13;
-		else while (str[i] >= 'N' && str[i] <= 'Z' || str[i] >= 'n' && str[i] <= 'z')
+		else while (str[i] >= 'N' && str[i] <= 'Z'
+		|| str[i] >= 'n' && str[i] <= 'z')
 		{
 			str[i] -= 13;
 			break;
 		}
 		i++;
 	}
-	return str;
+	return (str);
 }
