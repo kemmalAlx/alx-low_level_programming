117263b6fbc9bf0c0e10340d950911d12c79c37c
abdel-ke <kemmal1337@gmail.com>
2023-07-08 02:59:43 +0100

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,13 +11,13 @@
 
 int main(int ac, char **av)
 {
-	int num = atoi(av[1]), j, result;
+	int num = atoi(av[1]), j, result = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (ac != 2)
 		return (printf("Error\n"), 1);
 	if (num < 0)
-		printf("0\n");
+		return (printf("0\n"), 0);
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
