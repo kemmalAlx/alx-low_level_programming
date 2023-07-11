6e8525a6de1db34642a648033ad888d120123d37
abdel-ke <kemmal1337@gmail.com>
2023-07-11 01:03:17 +0100

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+int	ft_strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+	return count;
+}
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int j;
+	int sum;
+	int size;
+	char *concat;
+
+	if (!ac || !av)
+		return NULL;
+	sum = 0;
+	i = 0;
+	while (i < ac)
+	{
+		sum += ft_strlen(av[i]);
+		i++;
+	}
+	concat = malloc(sizeof(char) * (sum + 1));
+	if (!concat)
+		return NULL;
+	i = 0;
+	j = 0;
+	size = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			concat[size] = av[i][j];
+			j++;
+			size++;
+		}
+		concat[size] = '\n';
+		size++;
+		i++;
+	}
+	concat[size - 1] = '\0';
+	return (concat);
+}
