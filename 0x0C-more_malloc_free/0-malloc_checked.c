03f65369d41d87851d3da0cb47977f5e4ce475cb
abdel-ke <kemmal1337@gmail.com>
2023-07-13 22:59:49 +0100

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -15,28 +15,6 @@ void *malloc_checked(unsigned int b)
 
 	nbr = malloc(b);
 	if (!nbr)
-		exit (98);
+		exit(98);
 	return (nbr);
-}
-
-int main(void)
-{
-    char *c;
-    int *i;
-    float *f;
-    double *d;
-
-    c = malloc_checked(sizeof(char) * 1024);
-    printf("%p\n", (void *)c);
-    i = malloc_checked(sizeof(int) * 402);
-    printf("%p\n", (void *)i);
-    f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
-    d = malloc_checked(INT_MAX);
-    printf("%p\n", (void *)d);
-    free(c);
-    free(i);
-    free(f);
-    free(d);
-    return (0);
 }
