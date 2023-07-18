d3480546bc841f1aa3a68a9600fd34968c870718
abdel-ke <kemmal1337@gmail.com>
2023-07-18 01:34:56 +0100

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		d->name ? printf("Name: %s\n" , d->name) : puts("Name: (nil)");
+		d->name ? printf("Name: %s\n", d->name) : puts("Name: (nil)");
 		printf("Age: %f\n", d->age);
 		d->owner ? printf("Owner: %s\n", d->owner) : puts("Owner: (nil)");
 	}
