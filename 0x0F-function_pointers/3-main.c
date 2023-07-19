849858e3cf2c8f52dffef49c6656f729dd8eef58
abdel-ke <kemmal1337@gmail.com>
2023-07-19 23:05:29 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,12 +1,31 @@
 #include "3-calc.h"
 
+/**
+ * main - function main
+ *
+ * @ac: length of argument
+ * @av: values of arguments
+ *
+ * Return: return success or Error
+ */
 int main(int ac, char **av)
 {
-	if (ac == 4)
+	if (ac != 4)
 	{
-		if (av[2][0] == '+' || av[2][0] == '-'
-		|| av[2][0] == '*' || av[2][0] == '/' || av[2][0] == '%')
-			printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
+		printf("Error\n");
+		return (98);
 	}
+	if (av[2][0] != '+' && av[2][0] != '-'
+		&& av[2][0] != '*' && av[2][0] != '/' && av[2][0] != '%')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((av[2][0] == '/' || av[2][0] == '%') && atoi(av[3]) == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
 	return (0);
 }
