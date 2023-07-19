2edd419658671112cca70149af5c85debb49c090
abdel-ke <kemmal1337@gmail.com>
2023-07-19 23:31:34 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,24 +10,21 @@
  */
 int main(int ac, char **av)
 {
-	char *op = av[2];
-	int nbr1 = atoi(av[1]), nbr2 = atoi(av[3]);
-
 	if (ac != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	if (!get_op_func(op) || op[1])
+	if (!get_op_func(av[2]) || av[2][1])
 	{
 		printf("Error\n");
 		return (99);
 	}
-	if ((op[0] == '/' || op[0] == '%') && nbr2 == 0)
+	if ((av[2][0] == '/' || av[2][0] == '%') && atoi(av[3]) == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
-	printf("%d\n", get_op_func(op)(nbr1, nbr2));
+	printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
 	return (0);
 }
