f7fef75f77b9ec2e1df04c9616fdc1accbaaecc9
abdel-ke <kemmal1337@gmail.com>
2023-07-19 22:22:04 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,9 +2,11 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 3)
+	if (ac == 4)
+	{
 		if (av[2][0] == '+' || av[2][0] == '-'
 		|| av[2][0] == '*' || av[2][0] == '/' || av[2][0] == '%')
 			printf("%d\n", get_op_func(av[2])(atoi(av[1]), atoi(av[3])));
+	}
 	return (0);
 }
