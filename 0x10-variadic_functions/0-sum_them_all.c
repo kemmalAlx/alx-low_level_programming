69a03ca658f7caf76a0ca47e27d8caa9ab9125e3
abdel-ke <kemmal1337@gmail.com>
2023-07-21 00:49:53 +0100

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,12 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n <= 0)
-		return (0);
 	va_list ap;
 	unsigned int i = 0;
 	unsigned int sum = 0;
 
+	if (n <= 0)
+		return (0);
 	va_start(ap, n);
 	while (i < n)
 	{
