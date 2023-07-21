8f756548119b0640fb0b57333087c4b7bb43cfc5
abdel-ke <kemmal1337@gmail.com>
2023-07-21 02:32:41 +0100

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,32 +1,63 @@
 #include "variadic_functions.h"
 
+/**
+ * print_char - print a char
+ *
+ * @ap: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
 void print_char(va_list ap)
 {
 	printf("%c", va_arg(ap, int));
 }
 
+/**
+ * print_int - print an integer
+ *
+ * @ap: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
 void print_int(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
 
+/**
+ * print_float - print a float
+ *
+ * @ap: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
 void print_float(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
 
+/**
+ * print_string - print a string
+ *
+ * @ap: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
 void print_string(va_list ap)
 {
 	printf("%s", va_arg(ap, char *));
 }
 
 /**
- * print_strings - a function that prints string,
- * followed by a new line.
+ * print_all - a function that prints anything
  *
- * @separator: string to be printed between string
- * @n: number of integers passed to the function
- */
+ * @format: A string of character representing
+ *          the argument types
+*/
 void print_all(const char *const format, ...)
 {
 	va_list ap;
