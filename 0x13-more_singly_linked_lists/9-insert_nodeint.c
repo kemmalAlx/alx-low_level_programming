2c3b323ac47f223256364f69d7a96e8bb3d6f02d
Abdelhamid El kemmal <abdel-ke@e3r4p10.1337.ma>
2023-08-02 22:58:04 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     unsigned int pos = 1;
 
     new = malloc(sizeof(listint_t));
-    if (!new)
+    if (!new || !head)
         return (NULL);
     new->n = n;
     new->next = NULL;
-    if (!*head)
-        return (new);
     if (!idx)
     {
         new->next = *head;
