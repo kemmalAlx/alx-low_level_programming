52ddbfae0b4bec4939a2028fd44ce08c3c27cffa
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2023-08-08 11:20:18 +0100

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     int fd, r, r_p;
     char *buffer;
 
-    if (!filename)
+    if (!filename && letters)
         return (0);
     buffer = malloc(sizeof(char) * letters + 1);
     if (!buffer)
