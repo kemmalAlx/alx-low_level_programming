441225298fb1ecd2d358c8ad92f21e78c28cf2c6
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2023-08-08 11:23:35 +0100

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,17 +7,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
     if (!filename && letters)
         return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
     buffer = malloc(sizeof(char) * letters);
     if (!buffer)
         return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
     r = read(fd, buffer, letters);
-    if (r == -1)
+    close(fd);
+    if (r < 0)
     {
         free(buffer);
         return (0);
@@ -25,6 +23,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
     buffer[r] = '\0';
     r_p = write(1, buffer, r);
     free(buffer);
-    close(fd);
     return (r_p);
 }
