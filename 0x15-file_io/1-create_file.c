f230e5904cb47f9043defe80894b410e30d9a99e
Abdelhamid El kemmal <abdel-ke@e2r1p5.1337.ma>
2023-08-08 13:45:25 +0100

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * @text_content: a NULL terminated string to write to the file
  *
  * Return : 1 on success, -1 on failure (file can not be created,
- * file can not be written, write “fails”, etc…)
+ * 			file can not be written, write “fails”, etc…)
 */
 int create_file(const char *filename, char *text_content)
 {
@@ -27,5 +27,5 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	if (wr == -1)
 		return (-1);
-    return (1);
+	return (1);
 }
