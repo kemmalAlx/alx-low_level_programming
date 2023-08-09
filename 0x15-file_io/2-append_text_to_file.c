f77b4dfeb434fe901756379b628e35e72232efeb
abdel-ke <kemmalalx@gmail.com>
2023-08-09 01:48:59 +0100

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_len++;
 	if (text_len)
 		w = write(fd, text_content, text_len);
-	if (w < 0)
+	if (w == -1)
 		return (-1);
+	close(fd);
 	return (1);
 }
